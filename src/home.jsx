f99e434998d8e66fc6e8ce64e33df6714072b12b
unknown <santimallalakshmidhar@gmail.com>
2025-02-23 08:17:39 +0530

    first commit

---
+++ src/home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>Your Name</h1>
+            <p>Contact: your.email@example.com</p>
+        </div>
+    );
+}
+
+export default Home;
